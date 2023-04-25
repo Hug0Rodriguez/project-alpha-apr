14eca8ddaed22834d191c133a4a582bc5f79e14d
Hugo <6ornstar@gmail.com>
2023-04-25 11:59:57 -0400

    Complete Feature 7

---
+++ accounts/urls.py
@@ -0,0 +1,4 @@
+from django.urls import path
+from accounts import views
+
+urlpatterns = [path("login/", views.login_view, name="login")]
--- projects/views.py
+++ projects/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Project
+from projects.models import Project
 
 # Create your views here.
 
